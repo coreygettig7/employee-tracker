e7974a1c97efb4293d8122fb99d7d909ae8a5e8f
Corey Gettig <coreygettig7@gmail.com>
2022-03-04 23:26:19 -0500

    Added functions that allow the user to add a new employee, add a new role, and add a new department

--- index.js
+++ index.js
@@ -133,3 +133,162 @@ const viewEmployeesByDepartment = () => {
       });
   };
 
+// Add new employee to the database
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fistName',
+            message: "What is the employee's first name?",
+            validate: addFirstName => {
+                if (addFirstName) {
+                    return true;
+                } else {
+                    console.log('Please enter a first name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "What is the employee's last name?",
+            validate: addLastName => {
+                if (addLastName) {
+                    return true;
+                } else {
+                    console.log('Please enter a last name');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(answer => {
+    const nameGiven = [answer.fistName, answer.lastName]
+    const roleSql = `SELECT role.id, role.title FROM role`;
+    connection.query(roleSql, (error, data) => {
+        if (error) throw error; 
+        const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employee's role?",
+                choices: roles
+            }
+        ])
+        .then(roleChoice => {
+            const role = roleChoice.role;
+            nameGiven.push(role);
+            const managerSql =  `SELECT * FROM employee`;
+            connection.query(managerSql, (error, data) => {
+                if (error) throw error;
+                const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: "Who is the employee's manager?",
+                        choices: managers
+                    }
+                ])
+                .then(managerChoice => {
+                    const manager = managerChoice.manager;
+                    nameGiven.push(manager);
+                    const sql =   `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                                    VALUES (?, ?, ?, ?)`;
+                    connection.query(sql, nameGiven, (error) => {
+                        if (error) throw error;
+                        console.log("Employee has been added!")
+                        viewAllEmployees();
+                        });
+                    });
+                });
+            });
+        });
+    });
+};
+
+// Add a new role to the database
+const addRole = () => {
+    const sql = 'SELECT * FROM department'
+    connection.query(sql, (error, response) => {
+        if (error) throw error;
+        let deptNameArray = [];
+        response.forEach((department) => {deptNameArray.push(department.name);});
+        deptNameArray.push('Create Department');
+        inquirer
+            .prompt([
+                {
+                    name: 'departmentName',
+                    type: 'list',
+                    message: 'Which department is this new role in?',
+                    choices: deptNameArray
+                }
+            ])
+            .then((answer) => {
+                if (answer.departmentName === 'Create Department') {
+                    addDepartment();
+                } else {
+                    addRoleResume(answer);
+                }
+            });
+  
+        const addRoleResume = (departmentData) => {
+            inquirer
+                .prompt([
+                    {
+                        name: 'newRole',
+                        type: 'input',
+                        message: 'What is the name of your new role?',
+                    },
+                    {
+                        name: 'salary',
+                        type: 'input',
+                        message: 'What is the salary of this new role?',
+                    }
+                ])
+                .then((answer) => {
+                    let createdRole = answer.newRole;
+                    let departmentId;
+  
+                    response.forEach((department) => {
+                        if (departmentData.departmentName === department.name) {departmentId = department.id;}
+                    });
+  
+                    let sql =   `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+                    let newRole = [createdRole, answer.salary, departmentId];
+  
+                    connection.query(sql, newRole, (error) => {
+                        if (error) throw error;
+                        console.log("***************************************")
+                        console.log("New role has been created successfully!");
+                        console.log("***************************************")
+                        viewAllRoles();
+                    });
+                });
+        };
+    });
+};
+
+// Add a department to the database
+const addDepartment = () => {
+    inquirer
+        .prompt([
+        {
+            name: 'newDepartment',
+            type: 'input',
+            message: 'What is the name of your new Department?',
+        }
+    ])
+    .then((answer) => {
+        let sql =     `INSERT INTO department (name) VALUES (?)`;
+        connection.query(sql, answer.newDepartment, (error, response) => {
+            if (error) throw error;
+            console.log("**********************************************************");
+            console.log("The department has successfuly been added to the database!");
+            console.log("**********************************************************");
+            viewAllDepartments();
+        });
+    });
+};
